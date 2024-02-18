c965b15ad613fcad1138cbb63a79a29d1271d093
sunkk8482 <86597542+sunkk8482@users.noreply.github.com>
2024-02-18 20:24:38 +0900

    [Gold V] Title: 노드사이의 거리, Time: 60 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+using namespace std;
+
+struct node {
+	int dest;
+	int cost;
+};
+
+vector<node> v[1003];
+int dist[1003] = { 0, };
+
+int bfs(int start, int end) {
+	int sum = 0;
+	fill(dist, dist + 1003, -1);
+	queue<int> q;
+	q.push(start);
+	dist[start] = 0;
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+		for (auto i : v[now]) {
+			if (dist[i.dest] != -1) continue;
+			dist[i.dest] = dist[now] + i.cost;
+			q.push(i.dest);
+		}
+	}
+
+	return dist[end];
+}
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < N - 1; i++) {
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		v[from].push_back({ to,cost });
+		v[to].push_back({ from,cost });
+	}
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << bfs(a, b) << "\n";
+	}
+
+}
