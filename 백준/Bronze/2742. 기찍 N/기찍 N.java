c0b83b5abb559cd7f8dabd6cd76cc37e25eeb672
sunkk8482 <86597542+sunkk8482@users.noreply.github.com>
2024-11-19 00:07:06 +0900

    [Bronze IV] Title: 기찍 N, Time: 208 ms, Memory: 20788 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = n;i >= 1;i--){
+            bw.write(i+"");
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
