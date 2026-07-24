05dadc350308df4af2d6574dd3ab07883d4f3678
sunkk8482 <86597542+sunkk8482@users.noreply.github.com>
2026-07-24 16:08:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int org = x;
+    int sum = 0;
+    while (x != 0)
+    {
+        sum += x % 10;
+        x /= 10;
+    }
+    return (org % sum == 0) ? true : false;
+}
