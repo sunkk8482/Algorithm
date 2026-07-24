378cd8f7d7cb94a348258ccc43d4ab890fece0af
sunkk8482 <86597542+sunkk8482@users.noreply.github.com>
2026-07-24 16:33:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        answer += (signs[i] == true) ? absolutes[i] : (absolutes[i] * -1);
+    }
+    return answer;
+}
