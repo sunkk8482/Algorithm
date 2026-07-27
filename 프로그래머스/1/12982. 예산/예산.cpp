2951052dd01e9e7f34a570fe3a933f017246e995
sunkk8482 <86597542+sunkk8482@users.noreply.github.com>
2026-07-27 10:10:36 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    int sum = 0;
+    sort(d.begin(), d.end());
+    for (auto i : d)
+    {
+        sum += i;
+        if (sum > budget)
+        {
+            break;
+        }
+        answer++;
+    }
+    return answer;
+}
